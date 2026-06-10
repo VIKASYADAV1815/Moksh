6b4a4fdde20ff187a986a8e17f36ff9b175887a3
vikashyadav1815 <vikashyadav1815@gmail.com>
2026-06-10 14:23:54 +0530

    minor vervel error fix

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -17,7 +17,7 @@ const cardVariants = {
     opacity: 1,
     y: 0,
     scale: 1,
-    transition: { duration: 0.55, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.55, ease: "easeInOut" },
   },
 };
 
